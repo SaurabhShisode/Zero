c353afa8d2f8350910019e83c8a33cf825b05ada
SaurabhShisode <119099005+SaurabhShisode@users.noreply.github.com>
2026-01-18 20:25:05 +0530

    removed View Tiles and added welcome msg

--- frontend/src/dashboard/Topbar.tsx
+++ frontend/src/dashboard/Topbar.tsx
@@ -10,19 +10,11 @@ type Props = {
 
 };
 
-const VIEW_TITLES: Record<View, string> = {
-    daily: "Daily Problems",
-    company: "Company Wise",
-    topics: "Topic Wise",
-    placement: "Placement Mode",
-    discussions: "Discussions",
-    profile: "Profile",
-    settings: "Settings"
-};
+
 
 export default function Topbar({ view, scrollRef }: Props) {
     const logout = useAuthStore((s) => s.logout);
-
+    const user = useAuthStore(s => s.user)
     const [hidden, setHidden] = useState(false);
     const [border, setBorder] = useState(false);
     const [lastScrollY, setLastScrollY] = useState(0);
@@ -75,22 +67,16 @@ export default function Topbar({ view, scrollRef }: Props) {
 
 
             <div className="flex items-center justify-between px-8 py-5">
-                <h1 className="text-lg font-semibold font-geist tracking-tight">
-                    {VIEW_TITLES[view]}
+                <h1 className="text-lg  font-geist tracking-tight">
+                   <span className="text-gray-400">Welcome</span>{user?.name ? ` , ${user.name}` : ""}
                 </h1>
 
                 <div className="flex items-center gap-8 font-geist">
-                    <button className="text-white/60 hover:text-white transition">
-                        <Bell className="h-5 w-5" />
-                    </button>
-
-                    <button className="text-white/60 hover:text-white transition">
-                        <User className="h-5 w-5" />
-                    </button>
+                    
 
                     <button
                         onClick={logout}
-                        className="flex items-center gap-2 text-sm text-white/60 hover:text-white transition"
+                        className="flex items-center gap-2 text-sm text-black bg-white p-2 rounded-lg hover:text-white transition cursor-pointer hover:bg-red-500  "
                     >
                         <LogOut className="h-4 w-4" />
                         Logout
