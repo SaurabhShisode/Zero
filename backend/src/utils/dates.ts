3c31aa3c06ac6716b92c06df5df9fa3c66ed7dda
SaurabhShisode <119099005+SaurabhShisode@users.noreply.github.com>
2026-01-21 18:59:44 +0530

    Update dates.ts

--- backend/src/utils/dates.ts
+++ backend/src/utils/dates.ts
@@ -1,4 +1,10 @@
-import { startOfDay } from "date-fns";
+export const toDay = (date?: Date | string | number) => {
+  const base = date ? new Date(date) : new Date()
 
-export const toDay = (date?: Date | string | number) => startOfDay(date ? new Date(date) : new Date());
+  const istOffset = 5.5 * 60 * 60 * 1000
+  const istTime = new Date(base.getTime() + istOffset)
 
+  istTime.setHours(0, 0, 0, 0)
+
+  return new Date(istTime.getTime() - istOffset)
+}
