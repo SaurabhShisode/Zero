b2f8411641c1654986d84f62591036806f86a107
SaurabhShisode <119099005+SaurabhShisode@users.noreply.github.com>
2026-02-07 11:20:33 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
   }, [hydrate])
 
   return (
-    <>
+    <div className="bg-black">
       <Toaster
         position="top-right"
         toastOptions={{
@@ -39,6 +39,6 @@ export default function App() {
 
 
       <AppRouter />
-    </>
+    </div>
   )
 }
